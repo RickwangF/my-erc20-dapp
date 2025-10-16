789f807992e0d1ee19871e8e87410f5709b60d44
王维余 <wangweiyu@9ji.com>
2025-10-16 19:52:03 +0800

    add contract functions

--- src/app/lib/contract.ts
+++ src/app/lib/contract.ts
@@ -1,7 +1,8 @@
 // lib/contract.ts
 import {useAccount} from "wagmi";
-import { BrowserProvider, Contract, parseEther, formatEther } from 'ethers';
+import {BrowserProvider, Contract, parseEther, formatEther, ethers} from 'ethers';
 import abi from '../MetaNodeStakeABI.json';
+import erc20Abi from '../erc20ABI.json';
 
 async function getContract(address:string) {
     if (!window.ethereum) throw new Error('No wallet found');
@@ -10,8 +11,48 @@ async function getContract(address:string) {
     return new Contract(address, abi, signer);
 }
 
-export async function getStakedAmount(pid: number, address: string) {
+async function getTokenContract(address:string) {
+    if (!window.ethereum) throw new Error('No wallet found');
+    const provider = new BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    return new Contract(address, erc20Abi, signer);
+}
+
+export async function approve(tokenAddress: string, contractAddress: string, amount: number) {
+    debugger
+    const tokenContact = await getTokenContract(tokenAddress);
+    try {
+        const allowance: boolean = await tokenContact.approve(contractAddress, ethers.parseUnits(amount.toString(), 18));
+        return allowance;
+    } catch (error) {
+        console.error('Error during approval:', error);
+    }
+}
+
+export async function hasAdminRole(acountAddress: string, address: string) {
     const contract = await getContract(address);
+    try {
+        debugger
+        const isAdmin = await contract.hasRole(await contract.ADMIN_ROLE(), acountAddress);
+        return isAdmin;
+    } catch (error) {
+        console.error('Error checking admin role:', error);
+    }
+}
+
+export async function allowance(tokenAddress: string, owner: string, spender: string) {
+    const tokenContact = await getTokenContract(tokenAddress);
+    try {
+        const allowance = await tokenContact.allowance(owner, spender);
+        return formatEther(allowance);
+    } catch (error) {
+        console.error('Error fetching allowance:', error);
+    }
+}
+
+export async function getStakedAmount(address: string) {
+    const contract = await getContract(address);
+    const pid: number = await getPID(address) as number;
     const amount = await contract.stakingBalance(pid, address);
     return formatEther(amount);
 }
@@ -26,10 +67,22 @@ export async function getPID(address: string) {
     }
 }
 
-export async function stake(address: string, amount: string) {
-    const contract = await getContract(address);
-    const tx = await contract.stake({ value: parseEther(amount) });
-    return tx.wait();
+export async function stake(tokenAddress:string, contractAddress: string, amount: number) {
+    try {
+        const approved = await approve(tokenAddress, contractAddress, amount);
+        if (!approved) {
+            throw new Error('Approval failed');
+        }
+
+        debugger
+        const contract = await getContract(contractAddress);
+        const pid = await getPID(contractAddress);
+        const tx = await contract.deposit(pid, ethers.parseUnits(amount.toString(), 18));
+        const result = await tx.wait();
+        return result;
+    } catch (error) {
+        console.error('Error during staking:', error);
+    }
 }
 
 export async function redeem(address: string, amount: string) {
--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -2,18 +2,26 @@
 
 import styles from './stake.module.css'
 import {ConnectButton} from "@rainbow-me/rainbowkit";
-import { getStakedAmount, getPID, redeem, stake, withdraw } from '../lib/contract';
+import {getStakedAmount, getPID, redeem, stake, withdraw, approve, allowance, hasAdminRole} from '../lib/contract';
 import {useEffect, useState} from "react";
 import {useAccount} from "wagmi";
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+
 
 export default function Page() {
     const account = useAccount()
     const [stakeAmount, setStakeAmount] = useState('');
     const [loadedPID, setLoadedPID] = useState(false);
+    const [allowanceAmount, setAllowanceAmount] = useState('');
     const [approveContract, setApproveContract] = useState(false);
     const [pid, setPID] = useState<number | null>(null);
+    const [showAlert, setShowAlert] = useState(false);
+    const [stakeAmountInput, setStakeAmountInput] = useState('1');
 
     const contractAddress = "0x01A01E8B862F10a3907D0fC7f47eBF5d34190341"
+    const tokenAddress = "0x0918Bb9AD8a920Dbdbb814d5a379f7005fcbf90c"
 
     const fetchStakeAmount = async () => {
         console.log('Fetching staked amount for account:', account.address);
@@ -25,6 +33,32 @@ export default function Page() {
             console.error('Error fetching staked amount:', error);
         }
     };
+
+    const handleApprove = async () => {
+        console.log('Approving contract:', tokenAddress);
+        try {
+            const allowance = await approve(tokenAddress, contractAddress, 90);
+            console.log('Approval successful, allowance:', allowance);
+        } catch (error) {
+            console.error('Error during approval:', error);
+        }
+    }
+
+    const checkAllowance = async () => {
+        console.log('Checking allowance for contract:', tokenAddress);
+        try {
+            const currentAllowance = await allowance(tokenAddress, account.address as string, contractAddress);
+            setAllowanceAmount(currentAllowance);
+            console.log('Current allowance:', currentAllowance);
+            setShowAlert(true)
+            // 5秒后隐藏
+            setTimeout(() => setShowAlert(false), 5000);
+        } catch (error) {
+            console.error('Error checking allowance:', error);
+        }
+    }
+
+    const handleClose = () => setShowAlert(false);
 
     const fetchPID = async () => {
         console.log('Fetching PID');
@@ -38,15 +72,29 @@ export default function Page() {
         }
     };
 
-    useEffect(() => {
-        fetchPID()
-    }, [account.isConnected]);
+    const handleStakeOneToken = async () => {
+        console.log('Staking tokens');
+        try {
+            const tx = await stake(tokenAddress, contractAddress, 1);
+            console.log('Stake successful, transaction:', tx);
+        } catch (error) {
+            console.error('Error during staking:', error);
+        }
+    }
+
+    const handleCheckAdminRole = async () => {
+        console.log('Checking admin role for account:', account.address);
+        try {
+            const isAdmin = await hasAdminRole(account.address as string, contractAddress);
+            console.log('Is admin:', isAdmin);
+        } catch (error) {
+            console.error('Error checking admin role:', error);
+        }
+    }
 
     useEffect(() => {
-        if (pid !== null && loadedPID) {
-            fetchStakeAmount();
-        }
-    }, [pid, loadedPID]);
+        handleCheckAdminRole()
+    }, [account.isConnected]);
 
     return(
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -59,13 +107,26 @@ export default function Page() {
                         Stake Balance: { stakeAmount }
                     </p>
                     <div className="h-20">
-
+                        <Button variant="contained" onClick={ ()=> { checkAllowance() } }>fetch allowance amount</Button>
+                    </div>
+                    <div className="h-20">
+                        <Button variant="contained" onClick={ () => { handleStakeOneToken() }}> quick stake one token </Button>
                     </div>
                 </div>
             </main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 
             </footer>
+            <Snackbar
+                open={showAlert}
+                autoHideDuration={5000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    Allowance Amount: {allowanceAmount}
+                </Alert>
+            </Snackbar>
         </div>
     );
 }
